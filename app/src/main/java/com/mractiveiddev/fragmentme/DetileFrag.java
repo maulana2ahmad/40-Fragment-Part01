2cf4ff1121907667ba5f99af811c8d34baa7593b
maulana2ahmad <maulana2ahmad@gmail.com>
2019-01-04 11:38:21 +0700

    This is make Fragment-Part01

---
+++ app/src/main/java/com/mractiveiddev/fragmentme/DetileFrag.java
@@ -0,0 +1,23 @@
+package com.mractiveiddev.fragmentme;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class DetileFrag extends Fragment {
+
+    public DetileFrag() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detile, container, false);
+    }
+}
